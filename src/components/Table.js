b032fca45f334e281f08d425e898564d3233692d
emersonfbarros <emersonfb99@gmail.com>
2023-06-19 11:52:25 -0300

    requirement 7

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { FaEdit, FaTrash } from 'react-icons/fa';
 
 class Table extends Component {
   render() {
+    const { expenses } = this.props;
     return (
       <div className="table-wrapper">
         <table>
@@ -18,10 +22,51 @@ class Table extends Component {
               <th>Editar/Excluir</th>
             </tr>
           </thead>
+          <tbody>
+            {
+              expenses
+                .map(({ description, tag, method, value, currency, exchangeRates }) => (
+                  <tr key={ value }>
+                    <td>{ description }</td>
+                    <td>{ tag }</td>
+                    <td>{ method }</td>
+                    <td>{ Number(value).toFixed(2) }</td>
+                    <td>{ exchangeRates[currency].name }</td>
+                    <td>{ Number(exchangeRates[currency].ask).toFixed(2) }</td>
+                    <td>{ (value * exchangeRates[currency].ask).toFixed(2) }</td>
+                    <td>Real</td>
+                    <td>
+                      <button type="button" aria-label="Editar"><FaEdit /></button>
+                      <button type="button" aria-label="Excluir"><FaTrash /></button>
+                    </td>
+                  </tr>
+                ))
+            }
+          </tbody>
         </table>
       </div>
     );
   }
 }
 
-export default Table;
+Table.propTypes = {
+  expenses: PropTypes.arrayOf(
+    PropTypes.shape({
+      description: PropTypes.string.isRequired,
+      tag: PropTypes.string.isRequired,
+      method: PropTypes.string.isRequired,
+      value: PropTypes.string.isRequired,
+      currency: PropTypes.string.isRequired,
+      exchangeRates: PropTypes.objectOf(
+        PropTypes.shape({
+          name: PropTypes.string,
+          ask: PropTypes.string,
+        }),
+      ),
+    }),
+  ).isRequired,
+};
+
+const mapStateToProps = ({ wallet }) => ({ expenses: wallet.expenses });
+
+export default connect(mapStateToProps)(Table);
